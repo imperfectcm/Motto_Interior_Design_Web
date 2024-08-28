2492bb29b559df9a7dbf9c635a6f91515bb4cf74
unknown <imperfect.cm@gmail.com>
2024-08-28 15:24:50 +0800

    test tailwind

--- components/MainPageHeadContainer.tsx
+++ components/MainPageHeadContainer.tsx
@@ -2,11 +2,8 @@
 
 export default function MainPageHeadContainer() {
     return (
-        <section className="h-24 bg-neutral-900">
-            <button className="bg-indigo-500 ...">
-                Save changes
-            </button>
-            {/* <img className="h-24" src="/hardCodeImages/Project Chronology/00 cover.jpg" alt="Main Page Cover Image" /> */}
+        <section className="bg-neutral-900">
+            <img className="h-screen" src="/hardCodeImages/Project Chronology/00 cover.jpg" alt="Main Page Cover Image" />
         </section>
     );
 }
