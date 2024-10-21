70a910eb8f9477e2ddc08668a59cae89ae4da47c
unknown <imperfect.cm@gmail.com>
2024-10-21 16:58:02 +0800

    resave edit project code

--- controllers/images/index.ts
+++ controllers/images/index.ts
@@ -1,28 +1,6 @@
 import { uploadImagesToDBFailedToast } from "@/components/toastify/toast";
 
-export const uploadCoverImages = async (coverImageUrlList: string[], coverKeyList: string[], projectId: string) => {
-    if (!coverImageUrlList.length) return null;
-    try {
-        const res = await fetch("/api/cover-images", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                "projectId": projectId,
-                "coverImageUrlList": coverImageUrlList,
-                "coverKeyList": coverKeyList
-            }),
-        });
-        const resData = await res.json();
-        return resData.message;
-    } catch (error: any) {
-        uploadImagesToDBFailedToast();
-        throw new Error(error.message);
-    }
-}
-
-export const uploadImages = async (imageUrlList: string[], imageKeyList: string[], projectId: string) => {
+export const uploadImages = async (imageUrlList: string[], imageKeyList: string[], projectId: string, imageType: string = "image") => {
     if (!imageUrlList.length) return null;
     try {
         const res = await fetch("/api/project-images", {
@@ -31,9 +9,10 @@ export const uploadImages = async (imageUrlList: string[], imageKeyList: string[
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                "projectId": projectId,
-                "imageUrlList": imageUrlList,
-                "imageKeyList": imageKeyList
+                projectId,
+                imageUrlList,
+                imageKeyList,
+                imageType
             }),
         });
         const resData = await res.json()
@@ -41,5 +20,29 @@ export const uploadImages = async (imageUrlList: string[], imageKeyList: string[
     } catch (error: any) {
         uploadImagesToDBFailedToast();
         throw new Error(error.message);
+    }
+}
+
+export const deleteImages = async (imageList: any[]) => {
+    if (!imageList.length) return;
+    try {
+        const res = await fetch("/api/project-images", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                imageList: imageList
+            }),
+        });
+        if (!res.ok) {
+            const errorData = await res.json();
+            uploadImagesToDBFailedToast();
+            throw new Error(errorData.error || "Project images delete failed.");
+        }
+        const response = await res.json()
+        return response.message;
+    } catch (error) {
+        throw error;
     }
 }
