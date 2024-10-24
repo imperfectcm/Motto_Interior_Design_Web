e633ae2c36e2501689ab2bc7e44622d5d2a16ecf
unknown <imperfect.cm@gmail.com>
2024-10-24 15:29:24 +0800

    logo font

--- app/components/NavItems.tsx
+++ app/components/NavItems.tsx
@@ -16,7 +16,7 @@ const NavItems = (props: NavItemsProps) => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5, delay: 0.2, ease: 'easeOut' }}>
-            <Link className="col-span-2 col-start-5 nav-item text-2xl" href="/">MOTTO DEISGN</Link>
+            <Link className="col-span-2 col-start-5 nav-item text-2xl motto-name" href="/">MOTTO DEISGN</Link>
             <Link className="col-span-1 nav-item text-xl" href="/about">About<FontAwesomeIcon className="nav-arrow" icon={faArrowRight} /></Link>
             <Link className="col-span-1 nav-item text-xl" href="/projects">Portfolio<FontAwesomeIcon className="nav-arrow" icon={faArrowRight} /></Link>
             <Link className="col-span-1 nav-item text-xl" href="/contact">Contact<FontAwesomeIcon className="nav-arrow" icon={faArrowRight} /></Link>
--- app/projects/[name]/components/ImageContainer.tsx
+++ app/projects/[name]/components/ImageContainer.tsx
@@ -10,12 +10,12 @@ interface ImageContainerProps {
 const ImageContainer = (props: ImageContainerProps) => {
     const imageSource = props.imageSource;
     const ref = useRef(null);
-    const isInView = useInView(ref, { amount: 0.3, });
+    const inView = useInView(ref, { amount: 0.3, });
 
     return (
-        <motion.div ref={ref} animate={{ scale: isInView ? 1 : 0.8 }} transition={{ duration: 0.5 }} className="imgAni">
-            <section className="image-column" key={imageSource.id} >
-                <img src={`${imageSource.url}`} alt="Project image" className="image h-full object-contain" />
+        <motion.div ref={ref} animate={{ scale: inView ? 1 : 0.8 }} transition={{ duration: 0.5 }} className="image-column">
+            <section className="image" key={imageSource.id} >
+                <img src={`${imageSource.url}`} alt="Project image" className="h-full w-full object-contain" />
             </section>
         </motion.div>
     )
